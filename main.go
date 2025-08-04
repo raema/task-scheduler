2b27d5134d97dcefa8dd6b6c90d00ce6f9e455fc
Matthew Rae <mattrae@gmail.com>
2025-08-04 01:41:09 -0700

    add parse() to parse tasks and expectedTime() to calculate expected completion time

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
 func main() {
 
 }
@@ -8,4 +13,76 @@ type Task struct {
 	Name         string
 	Duration     int
 	Dependencies []string
+}
+
+func parse(input string) []Task {
+	if input == "" {
+		return []Task{}
+	}
+
+	var tasks []Task
+	lines := strings.Split(input, "\n")
+
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		fields := strings.Split(line, ",")
+		name := strings.TrimSpace(fields[0])
+		duration, err := strconv.Atoi(strings.TrimSpace(fields[1]))
+		if err != nil {
+			continue
+		}
+
+		var dependencies []string
+		for i := 2; i < len(fields); i++ {
+			dependency := strings.TrimSpace(fields[i])
+			dependencies = append(dependencies, dependency)
+		}
+
+		tasks = append(tasks, Task{
+			Name:         name,
+			Duration:     duration,
+			Dependencies: dependencies,
+		})
+
+	}
+	return tasks
+
+}
+
+func expectedTime(tasks []Task) int {
+	endTimeByTask := make(map[string]int)
+
+	for len(endTimeByTask) < len(tasks) {
+
+		for _, task := range tasks {
+			if _, done := endTimeByTask[task.Name]; done {
+				continue // task is done
+			}
+
+			ready := true
+			startTime := 0
+
+			for _, dependency := range task.Dependencies {
+				if endTime, done := endTimeByTask[dependency]; !done {
+					ready = false //dependency not done
+					break
+				} else if endTime > startTime {
+					startTime = endTime
+				}
+
+			}
+
+			if ready {
+				endTimeByTask[task.Name] = startTime + task.Duration
+			}
+		}
+	}
+
+	totalTime := 0
+	for _, endTime := range endTimeByTask {
+		if endTime > totalTime {
+			totalTime = endTime
+		}
+	}
+	return totalTime
 }
--- main_test.go
+++ main_test.go
@@ -22,3 +22,45 @@ func TestCreateTask(t *testing.T) {
 	}
 
 }
+
+func TestParseLineNoDependencies(t *testing.T) {
+	input := "Task 1, 10"
+	tasks := parse(input)
+
+	if tasks[0].Name != "Task 1" {
+		t.Errorf("Expected task name 'Task 1', got %s", tasks[0].Name)
+	}
+
+	if tasks[0].Duration != 10 {
+		t.Errorf("Expected task duration 10, got %d", tasks[0].Duration)
+	}
+}
+func TestParseLineWithDependencies(t *testing.T) {
+	input := "Task 2, 10, Task 1"
+	tasks := parse(input)
+
+	if tasks[0].Name != "Task 2" {
+		t.Errorf("Expected task name 'Task 2', got %s", tasks[0].Name)
+	}
+
+	if tasks[0].Duration != 10 {
+		t.Errorf("Expected task duration 10, got %d", tasks[0].Duration)
+	}
+
+	if tasks[0].Dependencies[0] != "Task 1" {
+		t.Errorf("Expected dependency 'Task 1' got %s", tasks[0].Dependencies[0])
+	}
+}
+
+func TestExpectedTime(t *testing.T) {
+	tasks := []Task{
+		{Name: "Task 1", Duration: 3, Dependencies: []string{}},
+		{Name: "Task 2", Duration: 2, Dependencies: []string{"Task 1"}},
+		{Name: "Task 3", Duration: 1, Dependencies: []string{"Task 1"}},
+		{Name: "Task 4", Duration: 4, Dependencies: []string{"Task 2", "Task 3"}},
+	}
+	time := expectedTime(tasks)
+	if time != 9 {
+		t.Errorf("Expected 9, got %d", time)
+	}
+}
